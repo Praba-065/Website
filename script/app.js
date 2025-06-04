170fde273cd8d1b4159617a6fb97feb8d29e9f4a
Prabavathi <prabhaelangovan2002@gmail.com>
2025-06-05 00:00:30 +0530

    added new file

--- script/app.js
+++ script/app.js
@@ -30,3 +30,55 @@ window.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+  
+	var carouselEle = $(".carousel-inner>.item>img");
+	var carouselEleLength = carouselEle.length;
+	if(carouselEleLength > 0) {
+		for(var i = 0; i < carouselEleLength; i++) {
+			$(carouselEle[i]).css({
+				'opacity': '0',
+			});
+			var imgurl = $(carouselEle[i]).attr("src");
+			$(carouselEle[i]).parent().css({
+				"background": "url(" + imgurl + ") center center no-repeat",
+				"background-size": "cover"
+			});
+		}
+	}
+	/*
+	Popup image
+	*/
+	var zoom_imageEle = $(".zoom_image");
+	var zoom_imageEleLength = zoom_imageEle.length;
+	if(zoom_imageEleLength > 0) {
+		for(var i = 0; i < zoom_imageEleLength; i++) {
+			$(zoom_imageEle[i]).magnificPopup({
+				type: 'image',
+				closeOnContentClick: true,
+				mainClass: 'mfp-with-zoom mfp-img-mobile',
+				image: {
+					verticalFit: true
+				}
+			});
+		}
+	}
+	/*
+	counter text
+	*/
+	var counterEle = $(".counter");
+	var counterEleLength = zoom_imageEle.length;
+	if(counterEleLength > 0) {
+		for(var i = 0; i < counterEleLength; i++) {
+			$(counterEle[i]).counterUp({
+				delay: 10,
+				time: 3000
+			});
+		}
+	}
+	$('.tweet').twittie({
+		dateFormat: '%B %d, %Y',
+		template: '{{tweet}} <span class="time">{{date}}</span>',
+		count: 5,
+		loadingText: 'Loading!'
+	});
+	/*
